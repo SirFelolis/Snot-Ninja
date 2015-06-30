0abc7f2c3762bec9b096f0d4aa21637c5efd062a
Oliver Thunæs <felolis30@gmail.com>
2015-06-30 20:13:25 +0200

    Added wall jump

--- Assets/Scripts/Behaviors/Jump.cs
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -5,12 +5,19 @@ public class Jump : AbstractBehavior
 {
     public float jumpSpeed = 100.0f;
     public float jumpBoostMultiplier = 2;
+    public float WallJumpBoostMultiplier = 5;
+    public float resetDelay = .2f;
+    public bool jumpingOffWall;
+    public Vector2 wallJumpSpeed = new Vector2(50, 150);
 
+    private float timeElapsed = 0;
     private float defaultJumpSpeed;
+    private Vector2 defaultWallJumpSpeed;
 
     void Start()
     {
         defaultJumpSpeed = jumpSpeed;
+        defaultWallJumpSpeed = wallJumpSpeed;
     }
 
     void Update()
@@ -20,11 +27,34 @@ public class Jump : AbstractBehavior
         var boost = _inputState.GetButtonValue(inputButtons[1]);
         var right = _inputState.GetButtonValue(inputButtons[2]);
         var left = _inputState.GetButtonValue(inputButtons[3]);
+        var onWall = _collisionState.onWall;
 
         if (_collisionState.standing)
         {
             if (canJump && holdTime < 0.1f)
-                OnJump();
+                OnNormalJump();
+        }
+        else if (!_collisionState.standing && onWall)
+        {
+            if ((canJump && holdTime < 0.1f) && !jumpingOffWall)
+            {
+                OnWallJump();
+
+                ToggleScripts(false);
+                jumpingOffWall = true;
+            }
+        }
+
+        if (jumpingOffWall)
+        {
+            timeElapsed += Time.deltaTime;
+
+            if (timeElapsed > resetDelay)
+            {
+                ToggleScripts(true);
+                jumpingOffWall = false;
+                timeElapsed = 0;
+            }
         }
 
         if (Input.GetButtonUp("Jump") && _rb2d.velocity.y > 0)
@@ -34,13 +64,26 @@ public class Jump : AbstractBehavior
             _rb2d.velocity = vel;
         }
         jumpSpeed = defaultJumpSpeed;
-        if (boost && (left || right))
-            jumpSpeed *= jumpBoostMultiplier;
+        wallJumpSpeed = defaultWallJumpSpeed;
+        if (boost)
+        {
+            wallJumpSpeed.x *= WallJumpBoostMultiplier;
+            if (left || right)
+            {
+                jumpSpeed *= jumpBoostMultiplier;
+            }
+        }
     }
 
-    protected virtual void OnJump()
+    protected virtual void OnNormalJump()
     {
         var vel = _rb2d.velocity;
         _rb2d.velocity = new Vector2(vel.x, jumpSpeed);
+    }
+
+    protected virtual void OnWallJump()
+    {
+        _inputState.direction = _inputState.direction == Directions.Right ? Directions.Left : Directions.Right;
+        _rb2d.velocity = new Vector2(wallJumpSpeed.x * (float)_inputState.direction, wallJumpSpeed.y);
     }
 }
