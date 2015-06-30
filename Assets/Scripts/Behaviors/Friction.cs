9536b05d676c0f9ec4da8fe24d0cfae5bb9cc252
Oliver Thunæs <felolis30@gmail.com>
2015-07-01 01:34:13 +0200

    Actually fixed crouching bug this time

--- Assets/Scripts/Behaviors/Friction.cs
+++ Assets/Scripts/Behaviors/Friction.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class Friction : AbstractBehavior
 {
-
-    void Start()
-    {
-    }
-
     void Update()
     {
         var right = _inputState.GetButtonValue(inputButtons[0]);
@@ -15,13 +10,18 @@ public class Friction : AbstractBehavior
         var jump = _inputState.GetButtonValue(inputButtons[2]);
         var down = _inputState.GetButtonValue(inputButtons[3]);
 
-        if ((!left && !right && !jump && _collisionState.standing) || (!left && !right && !jump && down && _collisionState.standing))
+        if ((!left && !right && !jump && _collisionState.standing))
         {
             _rb2d.drag = 10000;
         }
         else
         {
             _rb2d.drag = 0;
+        }
+
+        if (down && _collisionState.standing)
+        {
+            _rb2d.drag = 10000;
         }
     }
 }
