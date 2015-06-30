eef323317e99892cede7834ec144f5a0fc0eacda
Oliver Thunæs <felolis30@gmail.com>
2015-06-30 20:29:46 +0200

    Added a camera behavior
    Added camera behavior: center player

---
+++ Assets/Scripts/Camera Behaviors/PlayerCenter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCenter : MonoBehaviour
+{
+    public Transform player;
+    public float smooth = .2f;
+
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, player.position.x, smooth),
+            Mathf.Lerp(transform.position.y, player.position.y, smooth),
+            transform.position.z);
+
+        var pos = transform.position;
+
+        pos.x = (int)pos.x;
+        pos.y = (int)pos.y;
+    }
+}
