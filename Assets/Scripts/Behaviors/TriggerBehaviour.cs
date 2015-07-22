d02a10a0dd7577e5aec33652b7ce78caabae87e7
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-22 04:30:05 +0200

    Added trigger behaviour class

---
+++ Assets/Scripts/Behaviors/TriggerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    public enum TriggerType
+    {
+        KILL
+    }
+
+    public TriggerType type;
+    public string targetTag;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (type == TriggerType.KILL)
+        {
+            if (other.CompareTag(targetTag))
+                Destroy(other.gameObject);
+        }
+
+    }
+}
