739d5b4a7b08627afe6a625adfeefd5e40e3018c
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-23 21:32:44 +0200

    Cleaned up walk script

--- Assets/Scripts/Behaviors/Walk.cs
+++ Assets/Scripts/Behaviors/Walk.cs
@@ -4,29 +4,20 @@ public class Walk : AbstractBehavior
 {
     public float speed = 50.0f;
     public float runMultiplier = 1.0f;
-    public bool running;
     public bool moving;
 
 	void Update ()
     {
-        running = false;
         moving = false;
 
         var right = _inputState.GetButtonValue(inputButtons[0]);
         var left = _inputState.GetButtonValue(inputButtons[1]);
-        var run = _inputState.GetButtonValue(inputButtons[2]);
 
         if (right || left)
         {
             var currentSpeed = speed;
 
             moving = true;
-
-            if (run && runMultiplier > 0)
-            {
-                currentSpeed *= runMultiplier;
-                running = true;
-            }
 
             var velX = currentSpeed * Input.GetAxis("Horizontal");
 
