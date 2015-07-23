95407733aa5856e6ea432f66bdc5c010e13fb628
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-23 21:31:06 +0200

    Added drag behaviour

---
+++ Assets/Scripts/Behaviors/DragBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragBehaviour : AbstractBehavior
+{
+    public float dragInAir = 0.0f;
+
+    private float defaultDrag;
+
+    void Start()
+    {
+        defaultDrag = _rb2d.drag;
+    }
+
+    void Update()
+    {
+        if (!_collisionState.standing)
+            _rb2d.drag = dragInAir;
+        else
+            _rb2d.drag = defaultDrag;
+    }
+}
