8cd21b8091a8220c3ec5494b154b5d56dc8e0c77
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-23 03:06:57 +0200

    Added slide. Currently not used or working

---
+++ Assets/Scripts/Behaviors/Slide.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Slide : AbstractBehavior {
+
+    public float scale = 0.5f;
+    public bool sliding;
+    public float centerOffsetY = 0;
+
+    private CircleCollider2D _circleCollider;
+    private Vector2 _originalCenter;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _circleCollider = GetComponent<CircleCollider2D>();
+        _originalCenter = _circleCollider.offset;
+    }
+
+    protected virtual void OnSlide(bool value)
+    {
+        sliding = value;
+
+        ToggleScripts(!sliding);
+
+        var size = _circleCollider.radius;
+
+        float newOffsetY;
+        float sizeReciprocal;
+
+        if (sliding)
+        {
+            sizeReciprocal = scale;
+            newOffsetY = _circleCollider.offset.y - size / 2 + centerOffsetY;
+        }
+        else
+        {
+            sizeReciprocal = 1 / scale;
+            newOffsetY = _originalCenter.y;
+        }
+
+        size = size * sizeReciprocal;
+        _circleCollider.radius = size;
+        _circleCollider.offset = new Vector2(_circleCollider.offset.x, newOffsetY);
+    }
+
+    void Update()
+    {
+        var canSlide = _inputState.GetButtonValue(inputButtons[0]);
+        if (canSlide && _collisionState.standing && !sliding)
+        {
+            OnSlide(true);
+        }
+        else if (sliding && !canSlide)
+        {
+            OnSlide(false);
+        }
+    }
+}
