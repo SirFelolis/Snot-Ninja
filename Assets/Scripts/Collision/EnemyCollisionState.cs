6f978fc3b215f34863d182b588c87e92a843247a
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-25 23:37:09 +0200

    Added enemy collision state script

---
+++ Assets/Scripts/Collision/EnemyCollisionState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollisionState : MonoBehaviour
+{
+    public LayerMask collisionLayer;
+    public bool standing;
+    public bool onWall;
+    public Vector2 bottomPosition = Vector2.zero;
+    public Vector2 rightPosition = Vector2.zero;
+    public Vector2 leftPosition = Vector2.zero;
+    public float collisionRadius = 10.0f;
+    public Color debugCollisionColor = Color.red;
+
+    void FixedUpdate()
+    {
+        var pos = bottomPosition;
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+
+        standing = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+
+        onWall = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = debugCollisionColor;
+
+        var positions = new Vector2[] { rightPosition, bottomPosition, leftPosition };
+
+        foreach (var position in positions)
+        {
+            var pos = position;
+            pos.x += transform.position.x;
+            pos.y += transform.position.y;
+
+            Gizmos.DrawWireSphere(pos, collisionRadius);
+        }
+    }
+}
