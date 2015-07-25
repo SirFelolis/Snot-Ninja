57cf51240a6c29ec67e5a192387132532b92ec1d
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-25 23:36:21 +0200

    Added new camera behavior

---
+++ Assets/Scripts/Camera Behaviors/FollowPrediction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPrediction : MonoBehaviour
+{
+    public GameObject target; // Target has to have a rigidbody for the script to work
+    public float smooth = 0.2f;
+
+    private string _targetName;
+    private Rigidbody2D _Trb2d;
+
+    void Start()
+    {
+        _targetName = target.name;
+        _Trb2d = target.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (GameObject.Find(_targetName) != null)
+        {
+            transform.position = new Vector3(
+                Mathf.Lerp(transform.position.x, target.transform.position.x + _Trb2d.velocity.x / 4.0f, smooth),
+                Mathf.Lerp(transform.position.y, target.transform.position.y, smooth),
+                transform.position.z);
+
+            var pos = transform.position;
+
+            pos.x = (int)pos.x;
+            pos.y = (int)pos.y;
+
+            transform.position = pos;
+        }
+    }
+}
