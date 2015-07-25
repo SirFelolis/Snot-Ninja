5a9497ebaee58a46a7ea30ba5715b392d9847535
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-25 23:37:32 +0200

    Added basic movement script for enemy

---
+++ Assets/Scripts/Enemy Behaviors/Move.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyBehaviors
+{
+    Follow
+};
+
+public class Move : AbstractEnemyBehavior
+{
+    public Transform player;
+    public float speed = 50.0f;
+    public bool moving;
+    public EnemyBehaviors behavior;
+
+    void Update()
+    {
+        moving = false;
+
+        switch(behavior)
+        {
+            case EnemyBehaviors.Follow:
+                var target = ((player.position - transform.position).normalized * speed);
+
+                _rb2d.velocity = new Vector2(target.x, _rb2d.velocity.y);
+                moving = true;
+                break;
+        }
+
+        Debug.Log((player.position - transform.position).normalized * speed);
+    }
+}
