e783fbecd656890d0702c49b4d6c955abfa9bbbe
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-25 23:36:50 +0200

    Cleaned up script

--- Assets/Scripts/Camera Behaviors/PlayerCenter.cs
+++ Assets/Scripts/Camera Behaviors/PlayerCenter.cs
@@ -3,21 +3,24 @@
 public class PlayerCenter : MonoBehaviour
 {
     public Transform player;
-    public float smooth = .2f;
+    public float smooth = 0.2f;
 
     void Update()
     {
-        if (GameObject.Find("Player") != null)
+        if (GameObject.Find("Player") != null) //TODO: make it compatible with objects other than the player
         {
             transform.position = new Vector3(
                 Mathf.Lerp(transform.position.x, player.position.x, smooth),
                 Mathf.Lerp(transform.position.y, player.position.y, smooth),
                 transform.position.z);
-
-            var pos = transform.position;
-
-            pos.x = (int)pos.x;
-            pos.y = (int)pos.y;
         }
+
+        var pos = transform.position;
+
+        pos.x = (int)pos.x; // This gives the camera that NES feel when moving
+        pos.y = (int)pos.y;
+
+        transform.position = pos;
+
     }
 }
