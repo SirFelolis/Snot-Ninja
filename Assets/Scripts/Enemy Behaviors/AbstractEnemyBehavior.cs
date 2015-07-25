316ce3c76200af03174193e84b71e8344852bcd5
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-25 23:37:19 +0200

    Added abstract behavior script for enem

---
+++ Assets/Scripts/Enemy Behaviors/AbstractEnemyBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractEnemyBehavior : MonoBehaviour
+{
+    public MonoBehaviour[] disableScripts;
+
+    protected Rigidbody2D _rb2d;
+    protected EnemyCollisionState _enemyCollisionState;
+
+    protected Directions directions;
+
+    protected virtual void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _enemyCollisionState = GetComponent<EnemyCollisionState>();
+    }
+
+    protected virtual void ToggleScripts(bool value)
+    {
+        foreach (var script in disableScripts)
+        {
+            script.enabled = value;
+        }
+    }
+
+}
