7948ce015735e3085ca349bd08ee88fa44dab2f8
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-26 21:29:39 +0200

    Cleaned up script

--- Assets/Scripts/Enemy Behaviors/Move.cs
+++ Assets/Scripts/Enemy Behaviors/Move.cs
@@ -9,11 +9,14 @@ public enum EnemyBehaviors
 
 public class Move : AbstractEnemyBehavior
 {
-    public Transform player;
     public float speed = 50.0f;
+    public float sightRadius;
     public bool moving;
+    public bool vision;
     public EnemyBehaviors behavior;
+    public Transform player;
     public LayerMask layerMask;
+    public LayerMask canSee;
 
     void Start()
     {
@@ -24,17 +27,26 @@ public class Move : AbstractEnemyBehavior
     {
         moving = false;
 
+        vision = Physics2D.OverlapCircle(transform.position, sightRadius, canSee);
+
+        if (vision)
+            behavior = EnemyBehaviors.Follow;
+        else
+            behavior = EnemyBehaviors.Patrol;
+
+        RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, new Vector2(32 * (float)directions, -16), 32, layerMask);
+        RaycastHit2D hitWall = Physics2D.Raycast(transform.position, new Vector2(16 * (float)directions, 0), 16, layerMask);
         switch(behavior)
         {
             case EnemyBehaviors.Follow:
                 moving = true;
+                speed = 70.0f;
                 var target = ((player.position - transform.position).normalized);
                 _rb2d.velocity = new Vector2(Mathf.Sign(target.x) * speed, _rb2d.velocity.y);
                 break;
             case EnemyBehaviors.Patrol:
                 moving = true;
-                RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, new Vector2(32 * (float)directions, -16), 32, layerMask);
-                RaycastHit2D hitWall = Physics2D.Raycast(transform.position, new Vector2(16 * (float)directions, 0), 16, layerMask);
+                speed = 50.0f;
                 _rb2d.velocity = new Vector2((float)directions * speed, _rb2d.velocity.y);
                 if (hitFloor.collider == null)
                     Turn();
@@ -51,9 +63,13 @@ public class Move : AbstractEnemyBehavior
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(transform.position, new Vector2(32 * (float)directions, -32));
+        if (vision) Gizmos.color = Color.red;
+        else
+            Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
+
         Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, new Vector2(32 * (float)directions, -32));
         Gizmos.DrawRay(transform.position, new Vector2(16 * (float)directions, 0));
     }
 }
