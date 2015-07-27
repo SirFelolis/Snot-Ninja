15b150d2407e2797e4eec63845013072a2ebc737
Oliver Andreas Thunæs <felolis30@gmail.com>
2015-07-27 02:06:59 +0200

    Cleaned up script

--- Assets/Scripts/Collision/EnemyCollisionState.cs
+++ Assets/Scripts/Collision/EnemyCollisionState.cs
@@ -6,9 +6,11 @@ public class EnemyCollisionState : MonoBehaviour
     public LayerMask collisionLayer;
     public bool standing;
     public bool onWall;
+    public bool onLeftEdge;
+    public bool onRightEdge;
     public Vector2 bottomPosition = Vector2.zero;
-    public Vector2 rightPosition = Vector2.zero;
     public Vector2 leftPosition = Vector2.zero;
+    public Vector2 rightPosition = Vector2.zero;
     public float collisionRadius = 10.0f;
     public Color debugCollisionColor = Color.red;
 
@@ -20,14 +22,22 @@ public class EnemyCollisionState : MonoBehaviour
 
         standing = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
 
-        onWall = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+        pos = leftPosition;
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+        onLeftEdge = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+
+        pos = rightPosition;
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+        onRightEdge = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = debugCollisionColor;
 
-        var positions = new Vector2[] { rightPosition, bottomPosition, leftPosition };
+        var positions = new Vector2[] { leftPosition, bottomPosition, rightPosition };
 
         foreach (var position in positions)
         {
--- Assets/Scripts/Enemy Behaviors/Move.cs
+++ Assets/Scripts/Enemy Behaviors/Move.cs
@@ -25,6 +25,7 @@ public class Move : AbstractEnemyBehavior
 
     void Update()
     {
+
         moving = false;
 
         vision = Physics2D.OverlapCircle(transform.position, sightRadius, canSee);
@@ -34,9 +35,7 @@ public class Move : AbstractEnemyBehavior
         else
             behavior = EnemyBehaviors.Patrol;
 
-        RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, new Vector2(32 * (float)directions, -16), 32, layerMask);
-        RaycastHit2D hitWall = Physics2D.Raycast(transform.position, new Vector2(16 * (float)directions, 0), 16, layerMask);
-        switch(behavior)
+        switch (behavior)
         {
             case EnemyBehaviors.Follow:
                 moving = true;
@@ -47,11 +46,11 @@ public class Move : AbstractEnemyBehavior
             case EnemyBehaviors.Patrol:
                 moving = true;
                 speed = 50.0f;
-                _rb2d.velocity = new Vector2((float)directions * speed, _rb2d.velocity.y);
-                if (hitFloor.collider == null)
+                _rb2d.velocity = new Vector2((float)directions * speed, _rb2d.velocity.y); //TODO: FIX THIS. COMPLETELY BROKEN. HELP
+                if (!_enemyCollisionState.onLeftEdge || !_enemyCollisionState.onRightEdge)
+                {
                     Turn();
-                if (hitWall.collider != null)
-                    Turn();
+                }
                 break;
         }
     }
