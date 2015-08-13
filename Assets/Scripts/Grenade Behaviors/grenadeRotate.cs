4339b71ddd03511488d03c1c0cdd07bf6b279b6b
Oliver Thunæs <Felolis30@gmail.com>
2015-08-13 18:04:42 +0200

    Added new grenade

---
+++ Assets/Scripts/Grenade Behaviors/grenadeRotate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PixelArtRotation
+{
+    public class grenadeRotate : MonoBehaviour
+    {
+        public int rotationSpeed;
+        public bool grounded;
+        public LayerMask whatIsGround;
+
+        private PixelRotation _pixelRotation;
+
+        void Awake()
+        {
+            _pixelRotation = GetComponent<PixelRotation>();
+        }
+
+        void Update()
+        {
+            grounded = Physics2D.OverlapCircle(transform.position, 4, whatIsGround);
+
+            if (!grounded)
+            {
+                _pixelRotation.Angle += rotationSpeed;
+            }
+            else
+                _pixelRotation.Angle = 0;
+        }
+    }
+}
