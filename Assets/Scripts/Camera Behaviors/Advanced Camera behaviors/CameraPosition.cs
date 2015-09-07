d89b6f627fa50ca2f799686cd9d50f8c38c72976
Oliver Thunæs <Felolis30@gmail.com>
2015-09-07 23:17:48 +0200

    Cleaned camera scripts

--- Assets/Scripts/Camera Behaviors/Advanced Camera behaviors/CameraPosition.cs
+++ Assets/Scripts/Camera Behaviors/Advanced Camera behaviors/CameraPosition.cs
@@ -1,22 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum CAMERA_STATE
+{
+    FIXED,
+    SMART,
+}
+
 public class CameraPosition : MonoBehaviour
 {
-    public Vector2[] positions;
+    public Transform playerPos;
+    public Transform playerVelAnchor;
+    public float smooth;
 
-    private Vector2 totalPositions;
-    private Vector2 averagePosition;
-    void FixedUpdate()
+    [HideInInspector]
+    public CAMERA_STATE camState = CAMERA_STATE.SMART;
+    
+    [HideInInspector]
+    public Vector2 pointOfInterest = new Vector2();
+
+    private float startSmooth;
+    
+    private Vector2 cameraPosition = new Vector2();
+
+    void Awake()
     {
-        foreach (var pos in positions)
-            totalPositions += pos;
-
-        if (positions.Length > 0)
-        {
-            averagePosition = totalPositions / positions.Length;
-            transform.position = new Vector3(averagePosition.x, averagePosition.y, transform.position.z);
-        }
+        startSmooth = smooth;
     }
 
+    void FixedUpdate()
+    {
+        if (smooth != startSmooth)
+            smooth = startSmooth;
+            
+
+        if (camState == CAMERA_STATE.SMART)
+        {
+            Vector2 averagePos = (playerPos.position + playerVelAnchor.position) / 2;
+            cameraPosition = averagePos;
+        }
+        if (camState == CAMERA_STATE.FIXED)
+        {
+            smooth /= 1.5f;
+            cameraPosition = pointOfInterest;
+        }
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, cameraPosition.x, smooth), Mathf.Lerp(transform.position.y, cameraPosition.y, smooth), transform.position.z);
+    }
 }
