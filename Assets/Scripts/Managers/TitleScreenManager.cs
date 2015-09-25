8dc252812fcda34462715f4a5eece30689968790
Oliver Thunæs <Felolis30@gmail.com>
2015-09-25 20:45:52 +0200

    Added more options to main menu

--- Assets/Scripts/Managers/TitleScreenManager.cs
+++ Assets/Scripts/Managers/TitleScreenManager.cs
@@ -3,11 +3,27 @@ using System.Collections;
 
 public class TitleScreenManager : MonoBehaviour
 {
-    void Update()
+    public void StartGameNew() // Starts a new game
     {
-        if (Input.GetKeyUp(KeyCode.Return))
-        {
-            Application.LoadLevel(1);
-        }
+        Application.LoadLevel(2);
+    }
+
+    public void StartGameContinue() // Continues saved game
+    {
+    }
+
+    public void ExitGame() // Exits the game
+    {
+        Application.Quit();
+    }
+
+    public void Options() // Go to the options menu
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Return() // Returns back to the main menu (probably from the options)
+    {
+        Application.LoadLevel(0);
     }
 }
