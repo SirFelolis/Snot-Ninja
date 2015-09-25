e1cdcfc02ec8ceba52f07466a187a97fd0a55519
Oliver Thunæs <Felolis30@gmail.com>
2015-09-25 20:46:04 +0200

    Cleaned up script

--- Assets/Scripts/Player Behaviors/FastFall.cs
+++ Assets/Scripts/Player Behaviors/FastFall.cs
@@ -7,15 +7,11 @@ public class FastFall : AbstractBehavior
 {
     public float pullSpeed = -300;
 
-    void Start()
-    {
-    }
-
     void Update()
     {
         var down = _inputState.GetButtonValue(inputButtons[0]);
 
-        if (down && !_collisionState.standing && !_collisionState.onWall)
+        if (down && !_collisionState.standing && !_collisionState.onWall && _rb2d.velocity.y < -50)
         {
             _rb2d.velocity = new Vector2(_rb2d.velocity.x, pullSpeed);
         }
