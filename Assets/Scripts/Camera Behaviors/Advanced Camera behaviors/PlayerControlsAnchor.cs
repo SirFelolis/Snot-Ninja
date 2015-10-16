7c3c634d44eb082062a147c713b276f501c78ba0
Oliver Thunæs <Felolis30@gmail.com>
2015-10-17 01:49:57 +0200

    Tracked files.

---
+++ Assets/Scripts/Camera Behaviors/Advanced Camera behaviors/PlayerControlsAnchor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlsAnchor : MonoBehaviour
+{
+    public GameObject player;
+    public float smooth;
+    public float verticalNegateFactor;
+
+    void Update()
+    {
+        var pos = transform.position;
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            pos.x = Mathf.Lerp(pos.x, player.transform.position.x + Input.GetAxis("Horizontal") * 80, smooth);
+            pos.y = Mathf.Lerp(pos.y, player.transform.position.y + (Input.GetAxis("Vertical") * 80) / verticalNegateFactor, smooth);
+        }
+        transform.position = pos;
+    }
+
+}
---
+++ Assets/Scripts/NPC Behavior/ConversationBehavior.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConversationBehavior : MonoBehaviour
+{
+    public string[] dialogue;
+    public Text text;
+    public Image portrait;
+    public GameObject textPanel;
+
+    private bool canTalk = false;
+
+    private int index = 0; // How far are we into the dialogue?
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetAxis("Vertical") > 0.1 && !canTalk)
+            {
+                canTalk = true;
+                index = 0;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTalk = false;
+        }
+    }
+
+	void Update()
+	{
+        textPanel.SetActive(canTalk);
+        if (dialogue.Length > 0 && canTalk)
+        {
+            OnDialogue();
+        }
+    }
+
+    void OnDialogue()
+    {
+        text.text = dialogue[index];
+        if (Input.GetButtonUp("Fire2"))
+        {
+            if (index < (dialogue.Length - 1))
+            {
+                index++;
+            }
+            else
+            {
+                canTalk = false;
+            }
+        }
+    }
+}
