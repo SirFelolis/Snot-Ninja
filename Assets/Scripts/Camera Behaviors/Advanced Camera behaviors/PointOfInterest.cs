c8744795412063cf8e26a8e2d457f6f1944453e0
Oliver Thunæs <Felolis30@gmail.com>
2015-10-17 01:47:34 +0200

    Added zooming to the POI script

--- Assets/Scripts/Camera Behaviors/Advanced Camera behaviors/PointOfInterest.cs
+++ Assets/Scripts/Camera Behaviors/Advanced Camera behaviors/PointOfInterest.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class PointOfInterest : MonoBehaviour
 {
     public Transform target;
+    public float size = 100.0f;
+    [Range(0.0f, 1.0f)]
+    public float zoomSmooth = 0.2f;
 
+    private float _defaultSize;
     private CameraPosition _camPos;
 
     void Awake()
     {
         _camPos = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraPosition>();
+        _defaultSize = _camPos.GetComponent<Camera>().orthographicSize;
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            _camPos.GetComponent<Camera>().orthographicSize = Mathf.Lerp(_camPos.GetComponent<Camera>().orthographicSize, size, zoomSmooth);
             _camPos.camState = CAMERA_STATE.FIXED;
             _camPos.pointOfInterest = (Vector2)target.position;
         }
@@ -25,6 +31,7 @@ public class PointOfInterest : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            _camPos.GetComponent<Camera>().orthographicSize = _defaultSize;
             _camPos.camState = CAMERA_STATE.SMART;
             _camPos.pointOfInterest = new Vector2();
         }
