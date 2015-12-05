3a86f52e31f207197f9c6be4f3a7b7f857ab26cf
Oliver Thunæs <Felolis30@gmail.com>
2015-12-05 15:52:38 +0100

    Added new animations

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -9,13 +9,15 @@ public class PlayerManager : MonoBehaviour
     private Animator _animator;
     private CollisionState _collisionState;
     private Attack _attackBehavior;
+    private Rigidbody2D _rb2d;
 
     void Awake()
     {
         _inputState = GetComponent<InputState>();
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
         _collisionState = GetComponent<CollisionState>();
         _attackBehavior = GetComponent<Attack>();
+        _rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -32,14 +34,15 @@ public class PlayerManager : MonoBehaviour
                 ChangeAnimationState(1);
             }
 
-            if (_inputState.absVelY > 0.5f && !_collisionState.standing) // Jumping animation
+            if (_rb2d.velocity.y > 0.5f && !_collisionState.standing) // Jumping animation
             {
                 ChangeAnimationState(2);
             }
-        }
-        if (_attackBehavior.attacking) // Attacking animation
-        {
-            ChangeAnimationState(3);
+
+            if (_rb2d.velocity.y < 0.0f && !_collisionState.standing)
+            {
+                ChangeAnimationState(4);
+            }
         }
     }
 
