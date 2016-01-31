d40029baf8d0ff10080b7b845963c2fde06c9de3
Oliver Thunæs <Felolis30@gmail.com>
2016-01-31 04:35:59 +0100

    Added skidding

--- Assets/Scripts/Player Behaviors/Walk.cs
+++ Assets/Scripts/Player Behaviors/Walk.cs
@@ -8,15 +8,20 @@ public class Walk : AbstractBehavior
     [Header("Movement Fields")]
     public float speed = 50.0f;
     public float runMultiplier = 1.0f;
+    public float walkMultiplier = 0.5f;
     public float maxSpeed;
     public bool moving;
+    public bool slowMoving;
+    public bool skidding = false;
 
     void FixedUpdate()
     {
         moving = false;
+        slowMoving = false;
 
         var right = _inputState.GetButtonValue(inputButtons[0]);
         var left = _inputState.GetButtonValue(inputButtons[1]);
+        var walk = _inputState.GetButtonValue(inputButtons[2]);
 
         if (_collisionState.standing)
         {
@@ -24,11 +29,26 @@ public class Walk : AbstractBehavior
             {
                 var currentSpeed = speed;
 
+                if (walk)
+                {
+                    currentSpeed *= walkMultiplier;
+                    slowMoving = true;
+                }
+
                 moving = true;
 
                 var velX = currentSpeed * Input.GetAxis("Horizontal");
 
                 _rb2d.velocity = new Vector2(velX, _rb2d.velocity.y);
+            }
+
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && _inputState.absVelX > 0)
+            {
+                skidding = true;
+            }
+            else
+            {
+                skidding = false;
             }
         }
         else
